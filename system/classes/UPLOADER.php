5cdcb642b8f4869a60ee687fc99afe2ca8da8ebe
Hazar Artuner <hazar.artuner@gmail.com>
2013-05-29 10:03:47 +0300

    * uploadFile fonksiyonu ile ilgili bir hata giderildi.

--- system/classes/UPLOADER.php
+++ system/classes/UPLOADER.php
@@ -1,38 +1,39 @@
 <?php
 class PA_UPLOADER extends DB{
-	public $table;
-	public $error = "";
-	public $copyNameTag = "Kopya";
+    public $table;
+    public $error = "";
+    public $copyNameTag = "Kopya";
 
     private $public_root;
     private $private_root;
-	
-	function PA_UPLOADER(){
-		parent::DB();
-		
-		$this->table = $this->tables->file;
+
+    function PA_UPLOADER(){
+        parent::DB();
+
+        $this->table = $this->tables->file;
 
         global $public_uploadurl;
         global $private_uploadurl;
 
         $this->public_root = $public_uploadurl;
         $this->private_root = $private_uploadurl;
-	}
-	
-	function uploadFile($directory_id, $file=null, $rename = null, $access_type = "public"){
-		global $ADMIN;
-	    $root = $this->{$access_type . "_root"};
+    }
+
+    function uploadFile($directory_id, $file=null, $rename = null, $access_type = "public"){
+        global $ADMIN;
+        $root = $this->{$access_type . "_root"};
 
         $isStandartUpload = true;
-        $filename = $file["name"];
         $temporary_file = "";
 
         if(!is_array($file)){
             $isStandartUpload = false;
 
-            $fileInfo = (object)pathinfo($file);
-            $extension = strtolower($fileInfo->extension);
-            $filename = $fileInfo->basename;
+            $filename = basename($file);
+            $fileInfo = (object)getimagesize($file);
+
+            $extension = preg_replace("/.*?\//", "", $fileInfo["mime"]);
+            $extension = $extension == "jpeg" ? "jpg" : $extension;
 
             if(preg_match("/^((http\:)|(https\:)|(\/\/))/", $file) && ($remote_file = file_get_contents($file))){
                 // gecici dosya ismini hesapla ---------------------------------------
@@ -45,14 +46,26 @@ class PA_UPLOADER extends DB{
                 }
             }
         }
+        else{
+            $filename = $file["name"];
+            $fileInfo = (object)pathinfo($filename);
+            $extension = strtolower($fileInfo->extension);
+        }
 
         if(($rename != null) && (strlen($rename) > 2)){
+            if(preg_match("/\.[\w]*$/", $rename)){
+                $rename = preg_replace("/\.[\w]*$/", ".$extension", $rename);
+            }
+            else{
+                $rename .= ".$extension";
+            }
+
             $filename = $rename;
         }
 
-		$properties = $ADMIN->FILE->calculateFileProperties($directory_id, $filename, true, true, $access_type);
-		$thumb_file_id = $ADMIN->FILE->calculateThumbnailId($properties->extension);
-		$resolution = (object)array("width"=>0,"height"=>0);
+        $properties = $ADMIN->FILE->calculateFileProperties($directory_id, $filename, true, true, $access_type);
+        $thumb_file_id = $ADMIN->FILE->calculateThumbnailId($properties->extension);
+        $resolution = (object)array("width"=>0,"height"=>0);
 
         if($isStandartUpload){
             if($file["error"] != 0){
@@ -75,33 +88,31 @@ class PA_UPLOADER extends DB{
             }
         }
 
-		if($properties->type == "image"){
-			$ADMIN->IMAGE_PROCESSOR->load($root . $properties->url);
-			$resolution = $ADMIN->IMAGE_PROCESSOR->getResolution();
-		}
-		
-		if($file_id = $this->insert($this->table,array("basename"=>$properties->basename,
-												"filename"=>$properties->filename,
-												"directory_id"=>$properties->directory_id,
-												"url"=>$properties->url,
-												"type"=>$properties->type,
-												"extension"=>$properties->extension,
-												"size"=>$properties->size,
-												"creation_time"=>$properties->creation_time,
-												"last_update_time"=>$properties->last_update_time,
-												"width"=>$resolution->width,
-												"height"=>$resolution->height,
-												"thumb_file_id"=>$thumb_file_id,
-												"copied_file_id"=>$properties->copied_file_id,
-												"access_type"=>$access_type)))
-		{
+        if($properties->type == "image"){
+            $ADMIN->IMAGE_PROCESSOR->load($root . $properties->url);
+            $resolution = $ADMIN->IMAGE_PROCESSOR->getResolution();
+        }
+
+        if($file_id = $this->insert($this->table,array("basename"=>$properties->basename,
+            "filename"=>$properties->filename,
+            "directory_id"=>$properties->directory_id,
+            "url"=>$properties->url,
+            "type"=>$properties->type,
+            "extension"=>$properties->extension,
+            "size"=>$properties->size,
+            "creation_time"=>$properties->creation_time,
+            "last_update_time"=>$properties->last_update_time,
+            "width"=>$resolution->width,
+            "height"=>$resolution->height,
+            "thumb_file_id"=>$thumb_file_id,
+            "copied_file_id"=>$properties->copied_file_id,
+            "access_type"=>$access_type)))
+        {
             return $file_id;
-		}
-		else{
+        }
+        else{
             $this->error = "Database'e kaydedilemedi!";
             return false;
-		}
-	}
-	
-
+        }
+    }
 }
