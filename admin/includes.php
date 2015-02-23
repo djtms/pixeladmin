e6e8a48d6488f5c973cb2f4b13f736c2c329da39
Hazar Artuner <hazar.artuner@gmail.com>
2015-02-24 01:57:11 +0200

    * twig fix

--- admin/includes.php
+++ admin/includes.php
@@ -81,9 +81,24 @@ require_once __DIR__ . "/../vendor/autoload.php";
 // Register Twig
 $TwigLoader = new Twig_Loader_Filesystem();
 
+// Check twig cache directories, if not exists, create
+if(debug_mode){
+    $twigDirs = [
+        __DIR__ . '/../cache/',
+        __DIR__ . '/../cache/twig'
+    ];
+
+    foreach($twigDirs as $dir){
+        if(!is_dir($dir)){
+            mkdir($dir);
+        }
+    }
+}
+
 $Twig = new Twig_Environment($TwigLoader, array(
     'cache' => __DIR__ . '/../cache/twig',
 ));
+
 
 if(debug_mode){
     $Twig->clearCacheFiles();
